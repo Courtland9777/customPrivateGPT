fdead6e2df7d14e6345cf8c7a4a66039827ae8de
Courtland <Courtland9777@gmail.com>
2023-06-30 01:13:31 -0400

    boosting requirements versions

--- privateGPT.py
+++ privateGPT.py
@@ -12,15 +12,15 @@ import time
 load_dotenv()
 
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
+persist_directory = os.environ.get("PERSIST_DIRECTORY")
 
-model_type = os.environ.get('MODEL_TYPE')
-model_path = os.environ.get('MODEL_PATH')
-model_n_ctx = os.environ.get('MODEL_N_CTX')
-model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+model_type = os.environ.get("MODEL_TYPE")
+model_path = os.environ.get("MODEL_PATH")
+model_n_ctx = os.environ.get("MODEL_N_CTX")
+model_n_batch = int(os.environ.get("MODEL_N_BATCH",8))
+target_source_chunks = int(os.environ.get("TARGET_SOURCE_CHUNKS",4))
 # Added a paramater for GPU layer numbers
-n_gpu_layers = os.environ.get('N_GPU_LAYERS')
+n_gpu_layers = os.environ.get("N_GPU_LAYERS")
 
 from constants import CHROMA_SETTINGS
 
